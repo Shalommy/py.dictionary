11126de610efcfc3bb34aea3f60b361b1562ea5b
shaloomy <galoann@gmail.com>
2024-04-15 18:34:34 +0200

    mini-project

--- miniproject.py
+++ miniproject.py
@@ -1,7 +1,27 @@
 import json
+import difflib
+
+def load_dictionarry(data.jason):
 
 with open("data.json", "r") as file:
     data = json.load(file)
 
-    print(data)
+    def get_definition(word, dictionary):
+        word_lower = word.lower()
+        if word_lower in dictionary:
+            return dictionary[word_lower]
+
+            else: close_matches - diff.libget_close_matches(word_lower, dictionary.keys(), n=3)
+            if close_matches:
+                suggestions = ",".join(close_matches)
+                return f"Word not found. Did you mean: {suggestions}?"
+                else:
+                    return "Word not found in the dictionary."
+
+    #Usage
+    dictionary = load_dictionary("dictionary.json")
+    word = input("Enter a word:")
+    definition = get_definition(word, dictionary)
+    print(definition)
+
     
